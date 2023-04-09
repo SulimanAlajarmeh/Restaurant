5dcf698d40b8aeaa9564dbf6c6946e1b58ef32ed
Suliman Alajarmeh <101741978+SulimanAlajarmeh@users.noreply.github.com>
2023-04-09 20:13:57 +0300

    Add files via upload

--- AdmainArea/admain.php
+++ AdmainArea/admain.php
@@ -24,7 +24,7 @@ $admain_name=$_SESSION['admain_name'];
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../css/all.min.css">
     <link rel="stylesheet" href="../css/admain.css">
-    <title>Document</title>
+    <title>Admain DashBoard</title>
 </head>
 
 <body>
